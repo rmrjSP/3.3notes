5f3cb8acb11cae2e91849f720fdc53a1984c2338
Ramraj <ramrage13@gmail.com>
2022-05-05 08:54:15 -0700

    3.13

---
+++ index.js
@@ -0,0 +1,52 @@
+const cardList = document.querySelector('.cardList');
+const score = document.getElementById('score')
+let points = 0;
+buildBoard();
+
+let interval = setInterval(function(){
+    addCard(cardList.children.length + 1)
+}, 2000);
+
+function addCard(value){
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.classList.add('active');
+    card.innerHTML = value;
+    cardList.appendChild(card);
+}
+
+function buildBoard(){
+    for (let i=0; i<12; i++){
+        addCard('starter');
+    }
+}
+
+cardList.addEventListener('click', function(e){
+    console.log(e.target);
+    if (e.target.matches('.cardList')){
+        return
+    }
+    if (e.target.classList.contains('active')){
+        points +=1;
+        score.innerHTML = `score ${points}`;
+
+    }
+    if (e.target.classList.contains('inactive')) {
+        points += 2;
+        score.innerHTML = `score ${points}`;
+    }
+    if (e.target.classList.contains('active')){
+        e.target.classList.remove('active');
+
+        e.target.classList.add('inactive');
+        points +=1;
+        return
+    }
+    e.target.remove();
+    let children = cardList.children;
+    if (children.length < 1){
+        clearInterval(interval);
+        score.innerHTML = `total score ${points}`
+    }
+
+});
